9f2dbecf3dcecf77f13979328b9e37173eea6c15
Nicola Baldassarri <BALDASNI@PGWKS00069.dippp.interno.it>
2022-02-14 00:14:57 +0100

    creato menu e sidebar

--- MDC/App_Start/BundleConfig.cs
+++ MDC/App_Start/BundleConfig.cs
@@ -8,46 +8,41 @@ namespace MDC
         // Per altre informazioni sulla creazione di bundle, vedere https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //script
+            bundles.Add(new ScriptBundle("~/bundles/js/Mdc").Include(
+                        "~/Scripts/Mdc*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Utilizzare la versione di sviluppo di Modernizr per eseguire attività di sviluppo e formazione. Successivamente, quando si è
             // pronti per passare alla produzione, usare lo strumento di compilazione disponibile all'indirizzo https://modernizr.com per selezionare solo i test necessari.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/MdcSite.css"));
-
-            bundles.Add(new StyleBundle("~/bundles/css/MdcSubMenu").Include(
-                      "~/Content/MdcSubMenu.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/js/MdcSubMenu").Include(
-                      "~/Scripts/MdcSubMenu.js"));
-
-
-            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
-          "~/Scripts/popper.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/ListaUtenti/js").Include(
-          "~/DataTables/datatables.min.js",
-          "~/Scripts/ListaUtenti.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js/popper").Include(
+                        "~/Scripts/popper.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/js/bootstrap.table").Include(
-   "~/Scripts/bootstrap-table.js",
-   "~/Scripts/moment.min.js"
-   ));
+                       "~/Scripts/bootstrap-table.js",
+                       "~/Scripts/moment.min.js"
+                       ));
 
-            bundles.Add(new StyleBundle("~/Content/css/bootstrap.table").Include(
-               "~/Content/bootstrap-table.css"));
+            //css
+            bundles.Add(new StyleBundle("~/bundles/css/Mdc").Include(
+                      "~/Content/Mdc*"));
 
+            bundles.Add(new StyleBundle("~/bundles/css/bootstrap").Include(
+                      "~/Content/bootstrap-table.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/css/bootstrap-table").Include(
+                      "~/Content/bootstrap.css"));
         }
     }
 }
