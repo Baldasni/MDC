5a0a3222d0ac47834d449886065651b2309cb3bc
Nicola Baldassarri <BALDASNI@PGWKS00069.dippp.interno.it>
2022-02-15 07:53:46 +0100

    iniziato

--- MDC/Controllers/GestioneSociController.cs
+++ MDC/Controllers/GestioneSociController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MDC.Models;
+using MDC.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,55 @@ namespace MDC.Controllers
 {
     public class GestioneSociController : Controller
     {
+        private DatabaseContext db = new DatabaseContext();
+
         // GET: GestioneSoci/Ricerca
         public ActionResult Ricerca()
         {
-            return View();
+            RicercaSocioViewModel rsVM= new RicercaSocioViewModel();
+            return View(rsVM);
         }
+        // GET: GestioneSoci/Ricerca
+        [HttpPost]
+        public ActionResult Ricerca(RicercaSocioViewModel rsVM)
+        {
+            if (!String.IsNullOrEmpty(rsVM.CodiceFiscale))
+            {
+                string guid = CercaGuidFromCodFisc(rsVM.CodiceFiscale);
+                RedirectToAction("VisualizzaSocio", guid);
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(rsVM.Cognome) && !String.IsNullOrEmpty(rsVM.Nome))
+                    RedirectToAction("ListaSoci", "GestioneSoci", new { cognome = rsVM.Cognome, nome = rsVM.Nome });
+                else if (!string.IsNullOrEmpty(rsVM.Cognome))
+                    RedirectToAction("ListaSoci", "GestioneSoci", new { cognome = rsVM.Cognome });
+                else if (!string.IsNullOrEmpty(rsVM.Nome))
+                    RedirectToAction("ListaSoci", "GestioneSoci", new { nome = rsVM.Nome });
+                else
+                    rsVM.MessaggioErrore = "Inserire almento un valore";
+            }
+            return View(rsVM);
+        }
+
+        public ActionResult ListaSoci(string cognome, string nome)
+        {
+            ListaSociViewModel lsVM = new ListaSociViewModel();
+            lsVM.Cognome = cognome;
+            lsVM.Nome = nome;
+
+            lsVM.Soci = db.Soci.Where(s => s.Cognome == cognome && s.Nome == nome);
+
+            return View(lsVM);
+        }
+        private string CercaGuidFromCodFisc(string codFisc)
+        {
+            var socio = db.Soci.Where(s => s.CodiceFiscale == codFisc);
+            //return View(comuni.ToList());
+
+            return socio.FirstOrDefault().IdSocio.ToString();
+        }
+
         // GET: GestioneSoci/Inserimento
         public ActionResult Inserimento()
         {
---
+++ MDC/Models/ViewModels/ListaSociViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MDC.Models.ViewModels
+{
+    public class ListaSociViewModel
+    {
+
+        public String MessaggioErrore { get; set; }
+        public String MessaggioWorning { get; set; }
+        public String MessaggioInfo { get; set; }
+
+        public String Cognome { get; set; }
+
+        public String Nome { get; set; }
+
+        public ICollection<Socio> Soci { get; set; }
+    }
+}
